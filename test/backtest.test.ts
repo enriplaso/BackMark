d40bee1b79e72c64f8a571b424611e7a56499d34
git config --global user.email <enriplaso@gmail.com>
2021-10-10 11:15:39 +0200

    configure project

---
+++ test/backtest.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import 'mocha';
+describe('backtest tests', function () {
+
+    it('Should ...', async function () {
+
+        expect(true).be.true;
+    });
+
+});
