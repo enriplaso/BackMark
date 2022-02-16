54603c69ad12896bbc8beb435c236f36677cb79c
Enrique Plaza <enriplaso@gmail.com>
2022-02-16 21:54:13 +0100

    update config and types

--- src/lib/backtest.ts
+++ src/lib/backtest.ts
@@ -1,7 +1,46 @@
+import { IBacktest } from './IBacktest';
 import { ITradingData } from './models/ITradingData';
+import { createReadStream } from 'fs';
+import { IStrategy } from './IStrategy';
+import { once } from 'events'
 
-class Backtest {
-    constructor(data: ITradingData, stategy, fee?: number) {}
+export class Backtest implements IBacktest {
+
+    private readline = require('readline'); // Nodejs types for readline are missing
+
+    constructor(private tradingDataPath: string, private stategy?: IStrategy, private fee?: number) { }
+
+    init() {
+        throw new Error('Method not implemented.');
+    }
+
+    async backTest(): Promise<void> {
+        try {
+            const readInterface = this.readline.createInterface({
+                input: createReadStream(this.tradingDataPath),
+                //output: process.stdout,
+                console: false
+            });
+
+            let lines = 0;
+            readInterface.on('line', (line) => {
+                lines++;
+                if (lines > 1) {
+                    const clumnsArr = line.split(',');
+                    console.log(line);
+
+
+                    //   console.log(new Date(Number.parseInt(clumnsArr[0])));
+
+                }
+            });
+
+            await once(readInterface, 'close');
+            console.log('Reading file line by line with readline done.');
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
 }
 
-export default Backtest;
