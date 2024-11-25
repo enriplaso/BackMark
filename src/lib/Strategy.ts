92259fc64f0197d56e8ad82f9a9c51d8cb6ea38d
Enrique Plaza <enriplaso@gmail.com>
2024-11-25 18:48:21 +0100

    removed due to typo

--- src/lib/Strategy.ts
+++
@@ -1,7 +0,0 @@
-import { IExchangeClient } from './exchange/IExchangeClient';
-import { TradingData } from './exchange/types';
-
-export abstract class Strategy {
-    constructor(protected readonly exchangeClient: IExchangeClient) {}
-    abstract checkPosition(tradingData: TradingData): Promise<void>;
-}
