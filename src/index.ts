f0d65e2723ad99650a83b0a1a70e329856ecdf78
Enrique Plaza <enriplaso@gmail.com>
2024-11-27 19:42:10 +0100

    added .npmingnore & fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export * from './lib/backtest.js';
 export * from './lib/strategy.js';
+export * from './lib/types';
+export * from './lib/IBacktest';
+export * from './lib/orders/types';
+export * from './lib/exchange/types';
+export * from './lib/exchange/IExchangeClient';
